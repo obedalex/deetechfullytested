d4ea7612100cf058f7504a11f3be3c3aae634585
Your Name <obiney500@gmail.com>
2026-03-31 22:14:57 +0000

    feat: initial commit

---
+++ src/app/sections/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Next-Gen Tech, Delivered</h1>
+      <p>
+        Premium laptops, cutting-edge gadgets, and accessories — all curated for
+        the tech-obsessed.
+          </p>
+         
+    </div>
+  );
+}
+
+export default Hero
